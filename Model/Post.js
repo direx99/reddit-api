71ecd78814abf86755cc722998c5222f9cf27236
Dinith <codse202f060@student.nibm.lk>
2023-07-09 10:03:17 +0530

    post model and routes added

---
+++ Model/Post.js
@@ -0,0 +1,41 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    community:{
+        type:String,
+        required:true
+    },
+    time:{
+        type:String,
+        required:true
+    },
+    comments:{
+        type:Number,
+        required:false
+    },
+    likes:{
+        type:Number,
+        required:false
+    },
+    user:{
+        type:String,
+        required:true
+    }
+
+})
+ 
+
+
+
+
+
+
+
+const Post = mongoose.model("Post",postSchema);
+module.exports=Post;
---
+++ Routes/posts.js
@@ -0,0 +1,57 @@
+const router = require("express").Router();
+let Post = require("../Model/Post");
+
+router.route("/").post((req, res) => {
+    const title = req.body.title;
+    const community = req.body.community;
+    const time = req.body.time;
+    const comments = Number(req.body.comments);
+    const likes = Number(req.body.likes);
+    const user = req.body.user;
+
+    
+    
+    const newPost = new Post({
+        title,
+        community,
+        time,
+        comments,
+        likes,
+        user
+    })
+
+    newPost.save().then(() => {
+        res.json("Post added")
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+
+router.route("/").get((req, res) => {
+    Post.find().then((posts) => {
+        res.json(posts)
+    }).catch((err) => {
+        console.log(err);
+    })
+})
+
+
+router.get('/:id', (req, res) => {
+    const postId = req.params.id;
+    
+    Post.findById(postId)
+      .then(post => {
+        if (!post) {
+          return res.status(404).send('Post not found');
+        }
+        res.send(post);
+      })
+      .catch(err => {
+        res.status(500).send(err);
+      });
+  });
+  
+
+
+module.exports = router;
